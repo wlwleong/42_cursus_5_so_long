2a3e629f67dce1badb2d369790bec9a1213b44a8
William Low <wlwleong@gmail.com>
2022-07-14 14:04:05 +0800

    ft_strncmp.c included

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -1,29 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: wlow <wlow@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/07/14 21:10:30 by wlow              #+#    #+#             */
-/*   Updated: 2021/08/05 09:24:23 by wlow             ###   ########.fr       */
+/*   Created: 2021/07/25 18:10:25 by wlow              #+#    #+#             */
+/*   Updated: 2021/08/05 09:23:17 by wlow             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-# include <stdlib.h>
-# include <unistd.h>
-
-typedef struct s_list
+int	ft_strncmp(const char *str1, const char *str2, size_t count)
 {
-	void			*content;
-	struct s_list	*next;
-}				t_list;
+	size_t	i;
 
-char	**ft_split(char const *s, char c);
-int		ft_strncmp(const char *str1, const char *str2, size_t count);
-void	ft_putstr_fd(char *s, int fd);
-
-#endif
+	i = 0;
+	while (i < count && str1[i] && str2[i])
+	{
+		if (str1[i] != str2[i])
+			return ((unsigned char)str1[i] - (unsigned char)str2[i]);
+		i++;
+	}
+	if (i == count)
+		return (0);
+	return ((unsigned char)str1[i] - (unsigned char)str2[i]);
+}
