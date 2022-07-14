551d706aa9d24636c84051b15e91430a0a6f9db1
William Low <wlwleong@gmail.com>
2022-07-14 14:04:34 +0800

    file check updated

--- sources/so_long.c
+++ sources/so_long.c
@@ -13,22 +13,39 @@
 #include "../includes/so_long.h"
 #include <stdio.h>
 
+static int	ft_file_check(char *filename);
+
 int	main(int argc, char *argv[])
 {
-	char	**file;
-	int		i;
-
 	if (argc != 2)
 	{
 		printf("Invalid arguments\n");
 		exit(EXIT_FAILURE);
 	}
-	file = ft_split(argv[1], '.');
-	printf("Map name : %s\n", argv[1]);
-	printf("File type : %s\n", file[1]);
-	i = 0;
-	while (file[i])
-		free(file[i++]);
-	free(file);
+	if (ft_file_check(argv[1]))
+		printf("File type OK!\n");
+	else
+		printf("Invalid file type.\n");
 	return (0);
+}
+
+static int	ft_file_check(char *filename)
+{
+	char	**split_filename;
+	int		return_int;
+	int		i;
+
+	split_filename = ft_split(filename, '.');
+	i = 0;
+	while (split_filename[i])
+		i++;
+	if (ft_strncmp(split_filename[i - 1], "ber", 3) != 0)
+		return_int = 0;
+	else
+		return_int = 1;
+	i = 0;
+	while (split_filename[i])
+		free(split_filename[i++]);
+	free(split_filename);
+	return (return_int);
 }
