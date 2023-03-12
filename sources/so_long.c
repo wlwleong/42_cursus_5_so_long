1654803321881e6f2864243c667ea12eddacfa49
William Low <wlwleong@gmail.com>
2023-03-12 21:39:36 +0530

    map check updated

--- sources/so_long.c
+++ sources/so_long.c
@@ -37,24 +37,46 @@ int	main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 	if (!ft_map_check(fd))
+	{
+		printf("Error\n");
 		exit(EXIT_FAILURE);
+	}
 	return (0);
 }
 
 static int	ft_map_check(int fd)
 {
 	char	*map;
+	int		n_row;
+	size_t	n_col;
+	int		error_flag;
 
+	n_row = 0;
+	error_flag = 0;
 	map = get_next_line(fd);
+	n_col = ft_strlen(map);
 	printf("map data:\n");
 	while (map)
 	{
+		n_row++;
+		if (ft_strlen(map) != n_col)
+			error_flag = 1;
 		printf("%s", map);
 		free(map);
 		map = get_next_line(fd);
 	}
 	printf("\n");
 	free(map);
+	if (n_row < 3)
+	{
+		printf("map row < 3\n");
+		return (0);
+	}
+	else if (error_flag)
+	{
+		printf("inconsistent map width\n");
+		return (0);
+	}
 	return (1);
 }
 
